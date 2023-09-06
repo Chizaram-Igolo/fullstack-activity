8311524512ec055c2a61feda3695d3670388201c
Chizaram-Igolo <chizaram.igolo@yahoo.com>
2023-09-06 11:44:44 +0100

    ➕🏦 feat: Create a Simple Server with Node and Express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("d");
+import express from "express";
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send(`This is coming from app.get("/")`);
+});
+
+app.listen(3000, function () {
+  console.log(`Server running on port 3000`);
+});
